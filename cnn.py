c23916025da2e5e36ac2faf2298bd7b88bd52d03
JordanPickles <jordan.l.pickles@gmail.com>
2023-02-04 11:23:29 +0000

    updates cnn.py

--- cnn.py
+++ cnn.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 import torch 
 from torch.utils.data import Dataset, DataLoader, random_split
+import torch.nn.functional as F
 
+from torch.utils.tensorboard import SummaryWriter
 from tabular_data import load_airbnb
 
 
@@ -11,14 +13,16 @@ class AirbnbNightlyPriceImageDataset(Dataset):
     def __init__(self):
         super().__init__() 
         self.data = pd.read_csv('./airbnb-property-listings/tabular_data/clean_tabular_data.csv')
-        self.features, self.labels = load_airbnb(self.data, 'Price_Night')
-        assert len(self.features) == len(self.labels) # Data and labels have to be of equal length
+        self.X, self.y = load_airbnb(self.data, 'Price_Night')
+        assert len(self.X) == len(self.y) # Data and labels have to be of equal length
+        print(self.X.shape)
+        print(self.y.shape)
 
     def __getitem__(self, index):
-        return torch.tensor(self.features[index]), self.labels[index]
+        return (torch.tensor(self.X[index]), torch.tensor(self.y[index]))
 
     def __len__(self):
-        return len(self.features)
+        return len(self.X)
 
 
 dataset = AirbnbNightlyPriceImageDataset() #Creates an instance of the class
@@ -35,9 +39,51 @@ print(f"    Training: {len(train_dataset)}")
 print(f"    Validation: {len(validation_dataset)}")
 print(f"    Testing: {len(test_dataset)}")
 
-train_loader = DataLoader(dataset, batch_size = 64, shuffle=True)
+# Dataloaders for each set
+train_loader = DataLoader(train_dataset, batch_size = 4, shuffle=True)
+validation_loader = DataLoader(validation_dataset, batch_size = 16, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size = 16, shuffle=True)
+
+
+class LinearRegression(torch.nn.Module):
+    def __init__(self):
+        super().__init__() # initalises the parent class
+        self.linear_layer = torch.nn.Linear(12,1)
+
+    def forward(self, features):
+        return self.linear_layer(features) # Makes prediction
 
 
 
 
+# Function that trains the model
+def train(model, train_loader, epochs=10):
+    writer = SummaryWriter()
+    optimiser = torch.optim.SGD(model.parameters(),lr = 0.001)
+    batch_index = 0
 
+    for epoch in range(epochs): # Loops through the dataset a number of times
+        for batch in train_loader: # Samples different batches of the data from the data loader
+            features, labels = batch # Sets features and labels from the batch
+            features = features.type(torch.float32)
+            labels = labels.type(torch.float32)
+            labels = labels.unsqueeze(1) #Adds new dimension to tensor to ensure the labels and predictions are the same shape
+
+            
+            prediction = model(features) # Provides prediction through the linear regression model
+            loss = F.mse_loss(prediction, labels)
+            loss = loss.type(torch.float32) # Error for the linear regression
+            loss.backward() # Populates the gradients from the parameters of our model
+            print(loss.item())
+
+            optimiser.step() #Optimisation step
+            optimiser.zero_grad() # Resets the grad to zero as the grads are no longer useful as they were calculated when the model had different parameters
+
+            writer.add_scalar('loss', loss.item(), batch_index)
+            batch_index += 1
+
+
+
+
+model = LinearRegression()
+train(model, train_loader)
